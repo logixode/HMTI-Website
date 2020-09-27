eceefe1bab11fa26f41026efce9c2c7dbe080690
logixode <rohmad1230@gmail.com>
2020-09-28 01:38:08 +0700

    adding 404 page and comming soon page

---
+++ src/views/components/ComingSoon.vue
@@ -0,0 +1,18 @@
+<template>
+  <hero class="pb-8" />
+</template>
+
+<script>
+import Hero from "./Hero";
+export default {
+  components: {
+    Hero,
+  },
+};
+</script>
+
+<style>
+.pb-8 {
+  padding-bottom: 8rem;
+}
+</style>
---
+++ src/views/components/NotFound.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="profile-page">
+    <components-header></components-header>
+    <section class="section section-skew mt--150">
+      <div class="container">
+        <card shadow class="card-profile mt--400" no-body>
+          <div class="px-4 px-md-5">
+            <div class="mt-1 mt-md-3 py-3">
+              <div class="text-center py-5">
+                <h1 class="display-1">404 ERROR</h1>
+                <h4>- Maaf halaman yang anda tuju tidak ada... -</h4>
+              </div>
+            </div>
+          </div>
+        </card>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import ComponentsHeader from "./ComponentsHeader";
+export default {
+  components: {
+    ComponentsHeader,
+  },
+};
+</script>
